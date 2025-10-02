286bf7883b5c1e6b8dd2f6d7f04cd20ba39fd7e4
SambhavAthreyaGit <athreyasambhav@gmail.com>
2025-10-02 00:02:00 -0400

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const jetbrainsMono = JetBrains_Mono({
 
 export const metadata: Metadata = {
   title: "Moonshot",
-  description: "Plate Discipline Analysis for Baseball Coaches",
+  description: "Plate Discipline Analysis for Baseball Teams",
 };
 
 export default function RootLayout({
