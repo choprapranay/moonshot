46be465c105742102540df386383160e8c5a67bb
choprapranay <pranay.chopra@mail.utoronto.ca>
2025-11-15 16:20:09 -0500

    added a dataset loader to fetch data from database

---
+++ backend/services/dataset_loader_service.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from infrastructure.supabase_repository import SupabaseRepository
+
+class DatasetLoaderService:
+    def __init__(self):
+        self.repo = SupabaseRepository()
+
+    def get_training_dataset(self) -> pd.DataFrame:
+        df_2023 = self.repo.fetch_pitch_data("2023-03-30", "2023-10-01")
+        df_2024 = self.repo.fetch_pitch_data("2024-03-28", "2024-09-29")
+
+        # Merge the two seasons dataframes together
+        full_df = pd.concat([df_2023, df_2024], ignore_index=True)
+
+        return full_df
